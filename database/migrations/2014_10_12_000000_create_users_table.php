48ec6b3177923d7822f7cc3605599e73bb737e33
christine2519 <christinenokam4@gmail.com>
2021-07-22 16:25:01 +0200

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,8 +22,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('profile')->nullable();
-            $table->unsignedBigInteger('ville_id'); //required
-            $table->foreign('ville_id')->references('id')->on('villes')->onDelete('restrict')->onUpdate('cascade');
+            //$table->unsignedBigInteger('ville_id'); //required
+            //$table->foreign('ville_id')->references('id')->on('villes')->onDelete('restrict')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
